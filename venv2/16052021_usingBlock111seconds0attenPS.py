1e8de3b61f3f1b8e3165468d1b49ae9871067dd3
carlacodes <carla.griffiths@gmail.com>
2021-05-21 16:10:52 +0100

    Batch processing through all keyworded files that contain the word "arrays"

---
+++ venv2/16052021_usingBlock111seconds0attenPS.py
@@ -0,0 +1,198 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from affinewarp import ShiftWarping
+import os
+import h5py
+import numpy as np
+filepath = 'D:/Electrophysiological Data/F1702_Zola_Nellie/HP_BlockNellie-111/pitchshiftTarget/spikeArraysBlockNellie-111BB2andBB3atten0trialsMay-20-2021-12-01-01-671-PM.mat'
+arrays = {}
+f = h5py.File(filepath)
+for k, v in f.items():
+    newarray=np.array(v)
+    newarrayremove=newarray[0, :]
+    arrays[k] = newarrayremove
+fS=24414.065;
+#matplotlib inline
+# Trial duration and bin size parameters.
+TMIN = 0  # s
+TMAX = 1.2 # s
+BINSIZE = 0.01  # s
+NBINS = int((TMAX - TMIN) / BINSIZE)
+
+TMIN2=0
+TMAX2=1.2; #I made the maximum trial length 1.2 seconds
+# LFP parameters.
+LOW_CUTOFF = 10  # Hz
+HIGH_CUTOFF = 30  # Hz
+
+# Hyperparameters for shift-only warping model.
+SHIFT_SMOOTHNESS_REG = 0.5*1e-3
+SHIFT_WARP_REG = 1e-2*1e-3
+MAXLAG = 0.15*1e-3
+
+# Hyperparameters for linear warping model.
+LINEAR_SMOOTHNESS_REG = 1.0*1e-3
+LINEAR_WARP_REG = 0.065*1e-3
+
+from affinewarp import SpikeData
+
+# Spike times.
+S = dict(np.load("umi_spike_data.npz"))
+# data = SpikeData(
+#     trials=S["trials"],
+#     spiketimes=S["spiketimes"],
+#     neurons=S["unit_ids"],
+#     tmin=TMIN,
+#     tmax=TMAX,
+# )
+# result = arrays["oneDtrialIDarray"];
+# result = x[0, :, 0]
+data2=SpikeData(
+    trials=arrays["oneDtrialIDarray"],
+    spiketimes=arrays["oneDspiketimearray"],
+    neurons=arrays["oneDspikeIDarray"],
+    tmin=TMIN,
+    tmax=TMAX,
+)
+data2.n_neurons=data2.n_neurons.astype(np.int64)
+data2.n_trials=data2.n_trials.astype(np.int64)
+# Bin and normalize (soft z-score) spike times.
+binned = data2.bin_spikes(NBINS)
+binned = binned - binned.mean(axis=(0, 1), keepdims=True)
+binned = binned / (1e-2 + binned.std(axis=(0, 1), keepdims=True))
+
+# Crop spike times when visualizing rasters.
+cropped_data = data2.crop_spiketimes(TMIN, TMAX)
+
+# Load LFP traces (n_trials x n_timebins). Crop traces to [TMIN, TMAX).
+L = dict(np.load("umi_lfp_data.npz"))
+
+# Define bandpass filtering function for LFP
+from scipy.signal import butter, filtfilt, freqz
+
+def bandpass(x, lowcut, highcut, fs, order=5, axis=-1, kind='butter'):
+    """
+    Bandpass filters analog time series.
+
+    Parameters
+    ----------
+    x : ndarray
+        Time series data
+    lowcut : float
+        Defines lower frequency cutoff (e.g. in Hz)
+    highcut : float
+        Defines upper frequency cutoff (e.g. in Hz)
+    fs : float
+        Sampling frequency (e.g. in Hz)
+    order : int
+        Filter order parameter
+    kind : str
+        Specifies the kind of filter
+    axis : int
+        Axis along which to bandpass filter data
+    """
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    if kind == "butter":
+        b, a = butter(order, [low, high], btype="band")
+    else:
+        raise ValueError("Filter kind not recognized.")
+    return filtfilt(b, a, x, axis=axis)
+
+# Load LFP.
+L = dict(np.load("umi_lfp_data.npz"))
+
+# Apply bandpass filter.
+lfp = bandpass(L["lfp"], LOW_CUTOFF, HIGH_CUTOFF, L["sample_rate"])
+
+# Crop LFP time base to match spike times.
+tidx = (L["lfp_time"] >= TMIN) & (L["lfp_time"] < TMAX)
+lfp = lfp[:, tidx]
+lfp_time = L["lfp_time"][tidx]
+
+# Z-score LFP.
+lfp -= lfp.mean(axis=1, keepdims=True)
+lfp /= lfp.std(axis=1, keepdims=True)
+
+
+# Specify model.
+shift_model = ShiftWarping(
+    smoothness_reg_scale=SHIFT_SMOOTHNESS_REG,
+    warp_reg_scale=SHIFT_WARP_REG,
+    maxlag=MAXLAG,
+)
+
+# Fit to binned spike times.
+shift_model.fit(binned, iterations=50)
+
+# Apply inverse warping functions to data.
+shift_aligned_data = shift_model.transform(data2).crop_spiketimes(TMIN, TMAX)
+
+from affinewarp import PiecewiseWarping
+
+# Specify model.
+lin_model = PiecewiseWarping(
+    n_knots=0,
+    smoothness_reg_scale=LINEAR_SMOOTHNESS_REG,
+    warp_reg_scale=LINEAR_WARP_REG
+)
+
+# Fit to binned spike times.
+lin_model.fit(binned, iterations=50)
+
+# Apply inverse warping functions to data.
+linear_aligned_data = lin_model.transform(data2).crop_spiketimes(TMIN, TMAX)
+
+
+plt.plot(shift_model.loss_hist, label="shift")
+plt.plot(lin_model.loss_hist, label="linear")
+plt.xlabel("Iteration")
+plt.ylabel("Normalized Loss")
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+def make_space_above(axes, topmargin=1):
+    """ increase figure size to make topmargin (in inches) space for
+        titles, without changing the axes sizes"""
+    fig = axes.flatten()[0].figure
+    s = fig.subplotpars
+    w, h = fig.get_size_inches()
+
+    figh = h - (1-s.top)*h  + topmargin
+    fig.subplots_adjust(bottom=s.bottom*h/figh, top=1-topmargin/figh)
+    fig.set_figheight(figh)
+
+from affinewarp.visualization import rasters
+fig, axes=rasters(cropped_data, subplots=(5, 8));
+fig.suptitle('Original Data (Block106 Zola) ', fontsize=10, color='1', y='1')
+
+#plt.title('Rasters of Original Data (18/03/2021 Zola) ')
+plt.show() #original data
+
+fig, axes=rasters(shift_aligned_data, subplots=(5, 8));
+fig.suptitle(' Rasters after Shift Model (Block106 Zola) ', fontsize=10, color='1', y='1')
+
+#plt.title('Rasters after Shift Model (18/03/2021 Zola) ')
+plt.show()
+
+fig, axes= rasters(linear_aligned_data, subplots=(5, 8));
+fig.suptitle(' Rasters after Linear Model (Block106 Zola) ', fontsize=10, color='1', y='1')
+
+#make_space_above(axes, topmargin=10)
+
+#plt.title('Rasters after Linear Model (18/03/2021 Zola)')
+fig.tight_layout()
+fig.subplots_adjust(top=10)
+plt.show();
+BASE_PATH='D:/Electrophysiological Data/F1702_Zola_Nellie/HP_BlockNellie-111/'
+file_name='alignedDataBlock111ShiftModelPS'
+np.save(os.path.join(BASE_PATH, file_name), shift_aligned_data["spiketimes"])
+np.save(os.path.join(BASE_PATH, 'neuronIDsPS'), shift_aligned_data["neurons"])
+np.save(os.path.join(BASE_PATH, 'trialIDsPS'), shift_aligned_data["trials"])
+
+file_name2='alignedDataBlock111LinearModelPS'
+np.save(os.path.join(BASE_PATH, file_name2), linear_aligned_data["spiketimes"])
+np.save(os.path.join(BASE_PATH, 'linearModelneuronIDsPS'), linear_aligned_data["neurons"])
+np.save(os.path.join(BASE_PATH, 'linearModeltrialIDsPS'), linear_aligned_data["trials"])
---
+++ venv2/20052021_usingBlock111_seconds0attenCommonDistractor20PS.py
@@ -0,0 +1,221 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from affinewarp import ShiftWarping
+import os
+import h5py
+import numpy as np
+filepath = 'D:/Electrophysiological Data/F1702_Zola_Nellie/HP_BlockNellie-111/commondistractor20/PitchShift/spikeArraysBlockNellie-111BB2andBB3curratten0dist20May-20-2021-11-34-01-649-AM.mat'
+arrays = {}
+f = h5py.File(filepath)
+for k, v in f.items():
+    newarray=np.array(v)
+    newarrayremove=newarray[0, :]
+    arrays[k] = newarrayremove
+fS=24414.065;
+# filepath2 = 'D:/Electrophysiological Data/F1702_Zola_Nellie/HP_BlockNellie-106/commondistractor20/noPitchShift/spikeArraysBlockNellie-106BB2andBB3curratten0dist20May-18-2021-12-16-26-604-PM.mat'
+# arrays2={}
+# f2 = h5py.File(filepath2)
+# items2=f2.items()
+# for k2, v2 in f2.items():
+#     newarray2=np.array(v2)
+#     newarrayremove2=newarray2[0, :]
+#     arrays2[k2] = newarrayremove2
+#
+# filepath3 = 'D:/Electrophysiological Data/F1702_Zola_Nellie/HP_BlockNellie-110/commondistractor20/nopitchshift/spikeArraysBlockNellie-110BB2andBB3curratten0dist20May-18-2021-11-46-31-506-AM.mat'
+# arrays3 = {}
+# f3 = h5py.File(filepath3)
+# items3=f3.items()
+# for k3, v3 in f3.items():
+#     newarray3=np.array(v3)
+#     newarrayremove3=newarray3[0, :]
+#     arrays3[k3] = newarrayremove3
+#matplotlib inline
+# Trial duration and bin size parameters.
+TMIN = 0  # s
+TMAX = 1.2 # s
+BINSIZE = 0.01  # 10 ms
+NBINS = int((TMAX - TMIN) / BINSIZE)
+
+TMIN2=0
+TMAX2=1.2; #I made the maximum trial length 1.2 seconds
+# LFP parameters.
+LOW_CUTOFF = 10  # Hz
+HIGH_CUTOFF = 30  # Hz
+
+# Hyperparameters for shift-only warping model.
+SHIFT_SMOOTHNESS_REG = 0.5*1e-3
+SHIFT_WARP_REG = 1e-2*1e-3
+MAXLAG = 0.15*1e-3
+
+# Hyperparameters for linear warping model.
+LINEAR_SMOOTHNESS_REG = 1.0*1e-3
+LINEAR_WARP_REG = 0.065*1e-3
+
+from affinewarp import SpikeData
+
+# Spike times.
+#S = dict(np.load("umi_spike_data.npz"))
+# data = SpikeData(
+#     trials=S["trials"],
+#     spiketimes=S["spiketimes"],
+#     neurons=S["unit_ids"],
+#     tmin=TMIN,
+#     tmax=TMAX,
+# )
+# result = arrays["oneDtrialIDarray"];
+# result = x[0, :, 0]
+# adjustedTrial=arrays2["oneDtrialIDarray"]+max(arrays["oneDtrialIDarray"])
+# adjustedTrial2=arrays3["oneDtrialIDarray"]+max(adjustedTrial)
+# combinedTrials=np.concatenate((arrays["oneDtrialIDarray"], adjustedTrial, adjustedTrial2), axis=0)
+# combinedSpikeTimes=np.concatenate((arrays["oneDspiketimearray"], arrays2["oneDspiketimearray"], arrays3["oneDspiketimearray"]),axis=0)
+# combinedNeuron=np.concatenate((arrays["oneDspikeIDarray"], arrays2["oneDspikeIDarray"], arrays3["oneDspikeIDarray"]),axis=0)
+
+data2=SpikeData(
+    trials=arrays["oneDtrialIDarray"], #arrays["oneDtrialIDarray"],
+    spiketimes=arrays["oneDspiketimearray"], #["oneDspiketimearray"],
+    neurons=arrays["oneDspikeIDarray"], #["oneDspikeIDarray"],
+    tmin=TMIN,
+    tmax=TMAX,
+)
+data2.n_neurons=data2.n_neurons.astype(np.int64)
+data2.n_trials=data2.n_trials.astype(np.int64)
+# Bin and normalize (soft z-score) spike times.
+binned = data2.bin_spikes(NBINS)
+binned = binned - binned.mean(axis=(0, 1), keepdims=True)
+binned = binned / (1e-2 + binned.std(axis=(0, 1), keepdims=True))
+
+# Crop spike times when visualizing rasters.
+cropped_data = data2.crop_spiketimes(TMIN, TMAX)
+
+# Load LFP traces (n_trials x n_timebins). Crop traces to [TMIN, TMAX).
+L = dict(np.load("umi_lfp_data.npz"))
+
+# Define bandpass filtering function for LFP
+from scipy.signal import butter, filtfilt, freqz
+
+def bandpass(x, lowcut, highcut, fs, order=5, axis=-1, kind='butter'):
+    """
+    Bandpass filters analog time series.
+
+    Parameters
+    ----------
+    x : ndarray
+        Time series data
+    lowcut : float
+        Defines lower frequency cutoff (e.g. in Hz)
+    highcut : float
+        Defines upper frequency cutoff (e.g. in Hz)
+    fs : float
+        Sampling frequency (e.g. in Hz)
+    order : int
+        Filter order parameter
+    kind : str
+        Specifies the kind of filter
+    axis : int
+        Axis along which to bandpass filter data
+    """
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    if kind == "butter":
+        b, a = butter(order, [low, high], btype="band")
+    else:
+        raise ValueError("Filter kind not recognized.")
+    return filtfilt(b, a, x, axis=axis)
+
+# Load LFP.
+L = dict(np.load("umi_lfp_data.npz"))
+
+# Apply bandpass filter.
+lfp = bandpass(L["lfp"], LOW_CUTOFF, HIGH_CUTOFF, L["sample_rate"])
+
+# Crop LFP time base to match spike times.
+tidx = (L["lfp_time"] >= TMIN) & (L["lfp_time"] < TMAX)
+lfp = lfp[:, tidx]
+lfp_time = L["lfp_time"][tidx]
+
+# Z-score LFP.
+lfp -= lfp.mean(axis=1, keepdims=True)
+lfp /= lfp.std(axis=1, keepdims=True)
+
+
+# Specify model.
+shift_model = ShiftWarping(
+    smoothness_reg_scale=SHIFT_SMOOTHNESS_REG,
+    warp_reg_scale=SHIFT_WARP_REG,
+    maxlag=MAXLAG,
+)
+
+# Fit to binned spike times.
+shift_model.fit(binned, iterations=50)
+
+# Apply inverse warping functions to data.
+shift_aligned_data = shift_model.transform(data2).crop_spiketimes(TMIN, TMAX)
+
+from affinewarp import PiecewiseWarping
+
+# Specify model.
+lin_model = PiecewiseWarping(
+    n_knots=0,
+    smoothness_reg_scale=LINEAR_SMOOTHNESS_REG,
+    warp_reg_scale=LINEAR_WARP_REG
+)
+
+# Fit to binned spike times.
+lin_model.fit(binned, iterations=50)
+
+# Apply inverse warping functions to data.
+linear_aligned_data = lin_model.transform(data2).crop_spiketimes(TMIN, TMAX)
+
+
+plt.plot(shift_model.loss_hist, label="shift")
+plt.plot(lin_model.loss_hist, label="linear")
+plt.xlabel("Iteration")
+plt.ylabel("Normalized Loss")
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+def make_space_above(axes, topmargin=1):
+    """ increase figure size to make topmargin (in inches) space for
+        titles, without changing the axes sizes"""
+    fig = axes.flatten()[0].figure
+    s = fig.subplotpars
+    w, h = fig.get_size_inches()
+
+    figh = h - (1-s.top)*h  + topmargin
+    fig.subplots_adjust(bottom=s.bottom*h/figh, top=1-topmargin/figh)
+    fig.set_figheight(figh)
+
+from affinewarp.visualization import rasters
+fig, axes=rasters(cropped_data, subplots=(5, 8));
+fig.suptitle('Original Data (13-14/05 Zola) ', fontsize=10, color='1', y='1')
+
+#plt.title('Rasters of Original Data (18/03/2021 Zola) ')
+plt.show() #original data
+
+fig, axes=rasters(shift_aligned_data, subplots=(5, 8));
+fig.suptitle(' Rasters after Shift Model (13-14/05 Zola) ', fontsize=10, color='1', y='1')
+
+#plt.title('Rasters after Shift Model (18/03/2021 Zola) ')
+plt.show()
+
+fig, axes= rasters(linear_aligned_data, subplots=(5, 8));
+fig.suptitle(' Rasters after Linear Model (13-14/05 Zola) ', fontsize=10, color='1', y='1')
+
+#make_space_above(axes, topmargin=10)
+
+#plt.title('Rasters after Linear Model (18/03/2021 Zola)')
+fig.tight_layout()
+fig.subplots_adjust(top=10)
+plt.show();
+BASE_PATH='D:/Electrophysiological Data/F1702_Zola_Nellie/HP_BlockNellie-111/commondistractor20/PitchShift'
+file_name='alignedDataBlock111ShiftModeldistractor20PS'
+np.save(os.path.join(BASE_PATH, file_name), shift_aligned_data["spiketimes"])
+np.save(os.path.join(BASE_PATH, 'neuronIDsPS'), shift_aligned_data["neurons"])
+np.save(os.path.join(BASE_PATH, 'trialIDsPS'), shift_aligned_data["trials"])
+
+file_name='alignedDataBlock111LinearModeldistractor20PS'
+np.save(os.path.join(BASE_PATH, file_name), shift_aligned_data["spiketimes"])
+np.save(os.path.join(BASE_PATH, 'linearModelneuronIDsPS'), shift_aligned_data["neurons"])
+np.save(os.path.join(BASE_PATH, 'linearModeltrialIDsPS'), shift_aligned_data["trials"])
