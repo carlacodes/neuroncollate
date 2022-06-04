493697aaaf8952ff94049f4c4ad72b3e80e842d9
carlacodes <carla.griffiths@gmail.com>
2022-06-04 22:50:24 +0100

    removing cleanData function from lfp extraction

--- venv2/Zola/TargetOnset/Cruella/lfCruella_seconds0attentargetWordOnsetv2updatedLHSandRHS.py
+++ venv2/Zola/TargetOnset/Cruella/lfCruella_seconds0attentargetWordOnsetv2updatedLHSandRHS.py
@@ -8,6 +8,7 @@ import scipy.io as rd
 import mat73
 import seaborn as sns
 import scipy
+from scipy.signal import butter, lfilter
 from func_Cruella_seconds0attentargetWordOnsetv2updatedLHSandRHS import *
 
 #user_input = input('What is the name of your directory')
@@ -97,49 +98,6 @@ for k00 in pitch_shift_or_not:
 
         from affinewarp import SpikeData
 
-        # Spike times.
-        # S = dict(np.load("umi_spike_data.npz"))
-        # data = SpikeData(
-        #     trials=S["trials"],
-        #     spiketimes=S["spiketimes"],
-        #     neurons=S["unit_ids"],
-        #     tmin=TMIN,
-        #     tmax=TMAX,
-        # )
-        # result = arrays["oneDtrialIDarray"];
-        # result = x[0, :, 0]
-        # adjustedTrial = {}
-        # for i2 in range(len(blocksOfInterest2) - 1):
-        #     if i2 == 0:
-        #         adjustedTrial[i2] = blockData[blocksOfInterest2[i2 + 1]]["oneDtrialIDarray"] + max(
-        #             blockData[blocksOfInterest2[i2]]["oneDtrialIDarray"])
-        #     else:
-        #         adjustedTrial[i2] = blockData[blocksOfInterest2[i2 + 1]]["oneDtrialIDarray"] + max(
-        #             adjustedTrial[i2 - 1])
-        #
-        # if bool(adjustedTrial):
-        #     combinedTrialsAdjusted = np.concatenate([v for k, v in sorted(adjustedTrial.items())], 0)
-        #     firsttrialarray = blockData[blocksOfInterest2[0]]["oneDtrialIDarray"]
-        #     combinedTrials = np.append(firsttrialarray, combinedTrialsAdjusted)
-        # else:
-        #     combinedTrialsAdjusted = blockData[blocksOfInterest2[0]]["oneDtrialIDarray"]
-        #     # firsttrialarray = blockData[blocksOfInterest2[0]]["oneDtrialIDarray"]
-        #     combinedTrials = combinedTrialsAdjusted
-        #
-        # for i in range(len(combinedTrials)):
-        #     combinedTrials[i] -= 1
-        #
-        # combinedSpikeTimes=np.array([]); #declare empty numpy array
-        # combinedNeuron=np.array([])
-        # combinedLickReleaseTimes=np.array([])
-        #
-        # for i3 in range(len(blockData)):
-        #     selectedSpikeTimes = blockData[blocksOfInterest2[i3]]["oneDspiketimearray"]
-        #     selectedNeuronIDs = blockData[blocksOfInterest2[i3]]["oneDspikeIDarray"]
-        #     selectedLickReleaseIDs = blockData[blocksOfInterest2[i3]]["oneDlickReleaseArray"]
-        #     combinedSpikeTimes = np.append(combinedSpikeTimes, selectedSpikeTimes)
-        #     combinedNeuron = np.append(combinedNeuron, selectedNeuronIDs)
-        #     combinedLickReleaseTimes = np.append(combinedLickReleaseTimes, selectedLickReleaseIDs)
 
         #combinedSpikeTimes=np.concatenate([v for k,v in sorted(blockData.items())], key='oneDspiketimearray',  axis=0)
         TMAX =0.8*1000#max(combinedLickReleaseTimes) # s
@@ -154,131 +112,7 @@ for k00 in pitch_shift_or_not:
         TMAXzb =0.09*1000#max(combinedLickReleaseTimes) # s
         BINSIZEzb = 0.001*1000 #0.001*1000  # 10 ms
         NBINSzb = int((TMAXzb - TMINzb) / BINSIZEzb)
-        #adjustedTrial=arrays2["oneDtrialIDarray"]+max(arrays["oneDtrialIDarray"])
-        #adjustedTrial2=arrays3["oneDtrialIDarray"]+max(adjustedTrial)
-        # combinedTrials=np.concatenate((arrays["oneDtrialIDarray"], adjustedTrial, adjustedTrial2), axis=0)
-        # combinedSpikeTimes=np.concatenate((arrays["oneDspiketimearray"], arrays2["oneDspiketimearray"], arrays3["oneDspiketimearray"]),axis=0)
-        # combinedNeuron=np.concatenate((arrays["oneDspikeIDarray"], arrays2["oneDspikeIDarray"], arrays3["oneDspikeIDarray"]),axis=0)
 
-        # data2=SpikeData(
-        #     trials=combinedTrials, #arrays["oneDtrialIDarray"],
-        #     spiketimes=combinedSpikeTimes, #["oneDspiketimearray"],
-        #     neurons=combinedNeuron, #["oneDspikeIDarray"],
-        #     tmin=TMIN,
-        #     tmax=TMAX,
-        # )
-        #
-        #
-        # data2.n_neurons=data2.n_neurons.astype(np.int64)
-        # data2.n_trials=data2.n_trials.astype(np.int64)
-        #
-        # data2z=SpikeData(
-        #     trials=combinedTrials, #arrays["oneDtrialIDarray"],
-        #     spiketimes=combinedSpikeTimes, #["oneDspiketimearray"],
-        #     neurons=combinedNeuron, #["oneDspikeIDarray"],
-        #     tmin=TMINz,
-        #     tmax=TMAXz,
-        # )
-        #
-        #
-        # data2z.n_neurons=data2z.n_neurons.astype(np.int64)
-        # data2z.n_trials=data2z.n_trials.astype(np.int64)
-        #
-        # data2zb=SpikeData(
-        #     trials=combinedTrials, #arrays["oneDtrialIDarray"],
-        #     spiketimes=combinedSpikeTimes, #["oneDspiketimearray"],
-        #     neurons=combinedNeuron, #["oneDspikeIDarray"],
-        #     tmin=TMINzb,
-        #     tmax=TMAXzb,
-        # )
-        #
-        #
-        # data2zb.n_neurons=data2zb.n_neurons.astype(np.int64)
-        # data2zb.n_trials=data2zb.n_trials.astype(np.int64)
-        # # Bin and normalize (soft z-score) spike times.
-        # # Bin and normalize (soft z-score) spike times.
-        # binned = data2.bin_spikes(NBINS)
-        # binned = binned - binned.mean(axis=(0, 1), keepdims=True)
-        # binned = binned / (1e-2 + binned.std(axis=(0, 1), keepdims=True))
-        #
-        # trialrows= np.array([])
-        # maxcombinedTrials=int(max(combinedTrials))
-        # for i in range(maxcombinedTrials+1):
-        #     #trialrows.append((i)+1)
-        #     trialrows=np.append(trialrows,float(i))
-        #
-        # trialalignment=np.concatenate((combinedLickReleaseTimes.reshape(-1,1),trialrows.reshape(-1,1)),axis=1)
-        # #indTrial=np.argsort(trialalignment[:,0])
-        # sorted_array = trialalignment[np.argsort(trialalignment[:, 0])]
-        # sorted_array_trial=sorted_array[:,1]
-        # sorted_array_trial=(sorted_array_trial).astype(np.int)
-        # #t3 = np.concatenate((t1.reshape(-1,1),t2.reshape(-1,1),axis=1)
-        #
-        # #data3=data2.select_trials([1,2,3,4,5])
-        # #data4=data3.reorder_trials([0,1,3,2,4])
-        #
-        # data22=data2.reorder_trials(sorted_array_trial)
-        # # Bin and normalize (soft z-score) spike times.
-        # binnedLR = data2.bin_spikes(NBINS)
-        # binnedLR = binnedLR - binnedLR.mean(axis=(0, 1), keepdims=True)
-        # binnedLR = binnedLR / (1e-2 + binnedLR.std(axis=(0, 1), keepdims=True))
-        # #binnedLRStdDev=binnedLR.std(axis=(0, 1), keepdims=True)
-        #
-        # binnedLRz = data2z.bin_spikes(NBINSz)
-        # binnedmeans=binnedLRz.mean(axis=(0, 1), keepdims=True)
-        #
-        # binnedLRz = binnedLRz- binnedLRz.mean(axis=(0, 1), keepdims=True)
-        # binnedLRz = binnedLRz / (binnedLRz.std(axis=(0, 1), keepdims=True))
-        # binnedLRStdDev=binnedLRz.std(axis=(0, 1), keepdims=True)
-        #
-        # binnedLRzb = data2zb.bin_spikes(NBINSzb)
-        # binnedmeansb=binnedLRzb.mean(axis=(0, 1), keepdims=True)
-        #
-        # binnedLRzb = binnedLRzb- binnedLRzb.mean(axis=(0, 1), keepdims=True)
-        # binnedLRzb = binnedLRzb / (binnedLRzb.std(axis=(0, 1), keepdims=True))
-        # binnedLRStdDevb=binnedLRzb.std(axis=(0, 1), keepdims=True)
-        #
-        # neuronselect=[]
-        # neuronselectb=[]
-        # meanneuronselect=[]
-        # neuronselectmat= np.array([])
-        # neuronselectmatb= np.array([])
-        # selectedchantoadd=np.array([])
-        # neuronsbychan={}
-        # neuronsbychanb={}
-        #
-        # for i in range(len(binnedLRz)):
-        #     print(i)
-        #     neuronselect=binnedLRz[i]
-        #     neuronselectb=binnedLRzb[i]
-        #     for i2 in range(1,33):
-        #         selectedchantoadd=neuronselect[:,i2]
-        #         selectedchantoaddb=neuronselectb[:,i2];
-        #         #neuronselect2=neuronselectmat[i2]
-        #         neuronselectmat=np.append(neuronselectmat,selectedchantoadd, axis=0)
-        #         neuronselectmatb=np.append(neuronselectmatb, selectedchantoaddb, axis=0)
-        #         neuronsbychan[i2]=np.mean(neuronselectmat)
-        #         neuronsbychanb[i2]=np.mean(neuronselectmatb)
-        #         #meanneuronselect[i]=mean(neuronselect[i])
-        #
-        # goodChanlist=np.array([])
-        # binnedmeans=binnedmeans[0]
-        # binnedmeans=binnedmeans[0]
-        #
-        # binnedmeansb=binnedmeansb[0]
-        # binnedmeansb=binnedmeansb[0]
-        #
-        #
-        #
-        #
-        #
-        #
-        #
-        # cropped_data2 = data22.crop_spiketimes(TMIN, TMAX)
-        # # Crop spike times when visualizing rasters.
-        # cropped_data = data2.crop_spiketimes(TMIN, TMAX)
-        #
-        # # Load LFP traces (n_trials x n_timebins). Crop traces to [TMIN, TMAX).
         L = dict(np.load("umi_lfp_data.npz"))
         #
         # # Define bandpass filtering function for LFP
@@ -315,36 +149,43 @@ for k00 in pitch_shift_or_not:
             return filtfilt(b, a, x, axis=axis)
 
 
+        # def butter_bandpass(lowcut, highcut, fs, order=5):
+        #     return butter(order, [lowcut, highcut], fs=fs, btype='band')
+        #
+        #
+        # def butter_bandpass_filter(data, lowcut, highcut, fs, order=5, axis=-1):
+        #     b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+        #     y = lfilter(b, a, data, axis=axis)
+        #     return y
+        #
+
 
         # Fit to binned spike times.
         [shift_model, lin_model]=disgustingly_long_func(pitch_shift_or_not, left_hand_or_right, blocksOfInterest2)
         total_lfp_np=(np.array(total_lfp))
-        #total_lfp_modelfit=np.mean(total_lfp_modelfit, axis=0)
         total_lfp_np=np.mean(total_lfp_np, axis=2)
 
         fs=np.round(24414.0625*(1000/24414))
 
-
-
         ##making copy, z-scoring, then getting model fit
         total_lfp_modelfit=(np.array(total_lfp))
-        #total_lfp_modelfit=np.mean(total_lfp_modelfit, axis=2)
         #repeating S6 word for word, so I need to take the mean across channels and then add an extra dimension to make one "unit" for the spikedata object.
 
-        
 
+        for k in range(0, 32):
+            print(k)
+            chosensite=total_lfp_modelfit[:,:, k]
+            corresp_bp=bandpass(chosensite,  1,10, fs)
+            total_lfp_modelfit[:,:, k]=corresp_bp
 
+        total_lfp_modelfit=np.mean(total_lfp_modelfit, axis=2)
 
-
-        # for k in range(0, 32):
-        #     print(k)
-        #     chosensite=total_lfp_modelfit[:,:, k]
-        #     corresp_bp=bandpass(chosensite,  1,9, fs)
-        #     total_lfp_modelfit[:,:, k]=corresp_bp
-        #total_lfp_modelfit=bandpass(total_lfp_modelfit, 5,9, fs)
-
-        total_lfp_modelfit/= total_lfp_modelfit.std(axis=2, keepdims=True)
-        #total_lfp_modelfit=total_lfp_modelfit[:,:, np.newaxis]
+        total_lfp_modelfit /= total_lfp_modelfit.std(axis=(1), keepdims=True)
+        plt.plot( total_lfp_modelfit)
+        plt.show()
+        total_lfp_modelfit=total_lfp_modelfit[:,:, np.newaxis]
+        plt.plot(total_lfp_modelfit[:,:, 0])
+        plt.show()
 
 
         start = 0
@@ -455,10 +296,24 @@ for k00 in pitch_shift_or_not:
         corrcoeff=scipy.stats.pearsonr(fractional_shifts_spk_warp, fractional_shifts_lfp_warp)
         sns.scatterplot(x=fractional_shifts_spk_warp, y=fractional_shifts_lfp_warp)
         plt.show()
-
-
+        #
+        # L = dict(np.load("umi_lfp_data.npz"))
+        #
+        # # Apply bandpass filter.
+        # lfp = bandpass(L["lfp"], LOW_CUTOFF, HIGH_CUTOFF, L["sample_rate"])
+        #
+        # # Crop LFP time base to match spike times.
+        # tidx = (L["lfp_time"] >= TMIN) & (L["lfp_time"] < TMAX)
+        # lfp = lfp[:, tidx]
+        # lfp_time = L["lfp_time"][tidx]
+        #
+        # # Z-score LFP.
+        # lfp -= lfp.mean(axis=1, keepdims=True)
+        # lfp /= lfp.std(axis=1, keepdims=True)
+        #
+        
         fig2, axes2 = plt.subplots(1, 2, sharey=True, figsize=(10, 3.5))
-        axes2[0].imshow(total_lfp_np, **imkw)
+        axes2[0].imshow((total_lfp_modelfit), **imkw)
         axes2[1].imshow(lfp_sm_transf, **imkw)
 
         axes2[0].set_title("raw lfp (bandpass-filtered)")
@@ -470,7 +325,7 @@ for k00 in pitch_shift_or_not:
         fig, axes = plt.subplots(1, 2, figsize=(15, 5), sharey=True)
         fig.suptitle('LFP for f1815 cruella')
 
-        sns.lineplot(ax=axes[0], x=lfp_time_crop, y=lfp_np_plt)
+        sns.lineplot(ax=axes[0], x=lfp_time_crop, y=np.mean(total_lfp_modelfit[:,:,0], axis=0))
         sns.lineplot(ax=axes[1], x=lfp_time_crop, y=np.mean(lfp_sm_transf, axis=0))
         axes[0].set_title("raw lfp (bandpass-filtered)")
         axes[1].set_title("shift aligned-- fit on lfp")
